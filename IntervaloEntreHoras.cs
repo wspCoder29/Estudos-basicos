0afdaedc0c9693169b2ad7197d6a3e85121ce5b3
Leandro Ferreira <86746732+wspCoder29@users.noreply.github.com>
2022-03-15 16:26:48 -0300

    Add files via upload

---
+++ IntervaloEntreHoras.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Test84_Time
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Diferença entre duas datas: Dias, Horas, Minutos
+
+            string dtInicial = "01/01/2021 11:00";
+            DateTime dt = DateTime.ParseExact(dtInicial, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            string dtFinal = "01/02/2021 15:05";
+            DateTime dt2 = DateTime.ParseExact(dtFinal, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            TimeSpan tp = dt2.Subtract(dt);
+
+            Console.WriteLine("Entre as datas "+dtInicial+" e "+dtFinal);
+            Console.WriteLine("Dias: "+tp.Days);
+            Console.WriteLine("Horas: "+tp.Hours);
+            Console.WriteLine("Minutos: "+tp.Minutes);
+            
+
+        }
+    }
+}
